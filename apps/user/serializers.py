f5ae81c66e003935d217ce49cea8a87f198763be
Diego Bejar <diebejardelaguila@gmail.com>
2023-08-14 01:26:27 -0500

    Delete provider

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -5,6 +5,10 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UserSerializer(UserCreateSerializer):
+    def validate_password(self, value):
+        # Evitar la validación de contraseña
+        return value
+    
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = ('id', 'email', "img_profile",'first_name', 'phone', 'dni', 'last_name','expected_entry_time', 'expected_exit_time','is_active', 'is_staff')
